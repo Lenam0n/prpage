e3fdcd2d8ec6e74d72e8c6e0bf92fd2514a74f49
Lena <57621352+Lenam0n@users.noreply.github.com>
2024-11-01 10:37:06 +0100

    Update apiProvider.jsx

--- src/utils/apiProvider.jsx
+++ src/utils/apiProvider.jsx
@@ -4,7 +4,8 @@ const ApiContext = createContext();
 
 export const ApiProvider = ({ children }) => {
   //const apiBaseUrl = "http://192.168.178.177:3000";
-  const apiBaseUrl = "http://localhost:3001";
+  //const apiBaseUrl = "http://localhost:3001";
+  const apiBaseUrl = "https://portfoliobackend.lenam0n.net";
 
   return (
     <ApiContext.Provider value={apiBaseUrl}>{children}</ApiContext.Provider>
